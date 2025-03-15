38cfdfb40ef0273a10bb3459f8181e3a16e1f6f3
shoeiico <90294669+shoeiico@users.noreply.github.com>
2025-03-15 16:30:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv()
 app_base_path = os.getenv("APP_BASE_PATH", default=os.getcwd())
 data_base_path = os.path.join(app_base_path, "data")
 model_file_path = os.path.join(data_base_path, "model.pkl")
+scaler_file_path = os.path.join(data_base_path, "scaler.pkl")  # Added
 
 TOKEN = os.getenv("TOKEN", default="ETH").upper()
 TRAINING_DAYS = os.getenv("TRAINING_DAYS", default="90")
